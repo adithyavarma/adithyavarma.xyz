df549e8eb3150e8a8e8a9665de9cf02f5639ccae
AdithyaVarmaBh <durgaadithyavarma.bh@gmail.com>
2025-04-04 14:55:15 -0400

    updating branch

--- adithya/reservationSystem/reservationBackend/server.js
+++ adithya/reservationSystem/reservationBackend/server.js
@@ -4,13 +4,19 @@ const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 const port = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+const corsOptions={
+    origin: "https://adithyavarma.xyz",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true
+};
+app.use(cors(corsOptions));
 app.use(express.json());
+app.options("*", cors(corsOptions)); // Enable pre-flight requests for all routes
+// Middleware
+
 
 // PostgreSQL Connection
 const pool = new Pool({
